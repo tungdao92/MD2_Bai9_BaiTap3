226f5ed07e85451987b9faa93fe93f20d4e9186f
unknown <tungdao923@gmail.com>
2022-08-04 16:57:42 +0700

    bai3

---
+++ src/FizzBuzzTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    void testFizz3(){
+        int number = 3;
+        String expected = "Fizz";
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected, result);
+    }
+    @Test
+    void testFizz6(){
+        int number = 6;
+        String expected = "Fizz";
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected, result);
+    }
+    @Test
+    void testBuzz5(){
+        int number = 5;
+        String expected = "Buzz";
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected,result);
+    }
+    @Test
+    void testFizzBuzz15() {
+        int number = 15;
+        String expected = "FizzBuzz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected, result);
+    }
+
+}
